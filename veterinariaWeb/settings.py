5493d234f5a1b1da17bd73a4553a347a58a848ac
Alexander <jhon.montano0327@gmail.com>
2023-05-22 23:23:54 -0500

    Se crea la venta

--- veterinariaWeb/settings.py
+++ veterinariaWeb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import mysql.connector
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'veterinariamysql',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'alexander',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }   
@@ -133,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
