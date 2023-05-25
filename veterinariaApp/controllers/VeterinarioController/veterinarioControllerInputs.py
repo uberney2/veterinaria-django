6e6a3a608ad3532c76914e1c7daedc9c3f2879c0
uberneyPRFT <uberney.zapatap@perficient.com>
2023-05-25 15:20:21 -0500

    Agregar mascota

--- veterinariaApp/controllers/VeterinarioController/veterinarioControllerInputs.py
+++ veterinariaApp/controllers/VeterinarioController/veterinarioControllerInputs.py
@@ -1,4 +1,4 @@
-from veterinariaApp.controllers.VeterinarioController.veterinarioControllerBussines import AfiliarDueñoMascota
+from veterinariaApp.controllers.VeterinarioController.veterinarioControllerBussines import AfiliarDueñoMascota, afiliarMascota
 
 
 def AgregarDueñoMascota(cedulaDueño, nombreDueño, edad):
@@ -24,3 +24,53 @@ def AgregarDueñoMascota(cedulaDueño, nombreDueño, edad):
         return
     print("------------validacion exitosa---------------------")
     AfiliarDueñoMascota(nombreDueño, cedula, edad)
+    
+def AgregarMascota(nombre, cedulaDueño, edad, especie, raza, caracteristicas, peso):
+
+    if nombre == None or nombre == " ":
+        print("el nombre no puede ser un espacio vacio")
+        return
+    
+    if cedulaDueño == None or cedulaDueño == " ":
+        print("la cedula no puede ser vacio")
+        return
+    try:
+        cedula = int(cedulaDueño)
+    except:
+        print("la cedula debe ser numerica")
+        return
+    
+    if edad == None or edad == " ":
+        print("la edad no puede ser un espacio vacio")
+        return
+    try:
+        edad = int(edad)
+    except:
+        print("la edad debe ser numerica")
+        return
+    
+    if especie == None or especie == " ":
+        print("la especie no puede ser un espacio vacia")
+        return
+    
+    if raza == None or raza == " ":
+        print("la raza no puede ser un espacio vacia")
+        return
+    
+    if caracteristicas == None or caracteristicas == " ":
+        print("las caracteristicas no pueden ser un espacio vacio")
+        return
+    
+    if peso == None or peso == " ":
+        print("el peso no puede ser un espacio vacio")
+        return
+    try:
+        peso = int(peso)
+    except:
+        print("el peso debe ser numerica")
+        return
+    
+    print("------------validacion exitosa---------------------")
+    mascota = afiliarMascota(nombre, cedula, edad, especie, raza, caracteristicas, peso)
+    
+    return
