12f3b85246d98f7d8ca69d0bcba3c3acc419c228
Andrespc <anfega@outlokk.es>
2023-05-27 13:38:22 -0500

    new features

--- veterinariaApp/controllers/AdminitradorController/AdministradorBussines.py
+++ veterinariaApp/controllers/AdminitradorController/AdministradorBussines.py
@@ -26,3 +26,19 @@ def addEmpleado(nombre, cedula, edad, rol, usuario, contraseña):
         )
         nuevo_usuario.save()
         return nuevo_usuario
+   
+Roles = {
+        "1": "Administrador",
+        "2": "Veterinario",
+        "3": "Vendedor",
+        "4": "Dueño de mascota"
+    }
+def lookAll():
+    
+    users = Usuario.objects.all()
+
+    for user in users:
+         rol = user.rol_id
+         user.rol_id = Roles.get(rol)
+
+    return users
--- veterinariaApp/urls.py
+++ veterinariaApp/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns=[
-    path('', views.iniciar, name="inicio"),
-    path('register/', views.register ),
-    path('singup/',views.singup),
-    path('administrator/', views.administrator ),
-    path('singup/logout/',views.logout),
+    path('', views.iniciar, name='inicio'),
+    path('register/', views.register, name='register'),
+    path('singup/',views.singup, name='singup'),
+    path('administrator/', views.administrator, name='admin' ),
+    path('singup/logout/',views.logout,name='logout'),
     path('hc/<str:id>', views.crearHistoriaClinica, name = 'hc' ),
     path('dueño-mascota/', views.crearDueñoMascota, name = 'dueño' ),
     path('mascota/', views.crearMascota, name = 'mascota' ),
@@ -17,5 +17,8 @@ urlpatterns=[
     path('listar-historia/<str:id>', views.listarHistoriaClinica, name = 'listar-historia' ),
     path('editar-historia-clinica/<str:id>', views.edicionHistoriaClinica, name = 'editar-historia-clinica' ),
     path('cancelar-orden/', views.cancelarOrden, name = 'cancelar-orden' ),
-    path('confirmacion-cancelar-orden/<str:id>', views.confirmacionCancelacionOrden, name = 'confirmacion-cancelar-orden' )
+    path('confirmacion-cancelar-orden/<str:id>', views.confirmacionCancelacionOrden, name = 'confirmacion-cancelar-orden' ),
+    path('error/<str:user_id>/delete', views.eliminarUser, name = 'delete' ),
 ]
+
+ 
