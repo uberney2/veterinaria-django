b01d5b11543a5cb9687aded2925941b97d2fb841
uberney2 <uzapatap@gmail.com>
2023-05-27 01:23:03 -0500

    Revert "características nuevas"

--- veterinariaApp/urls.py
+++ veterinariaApp/urls.py
@@ -1,17 +1,14 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
-import debug_toolbar;
 
 
 urlpatterns=[
-     path('__debug__/', include(debug_toolbar.urls)),
-    path('', views.iniciar, name='inicio'),
-    path('register/', views.register, name='register'),
-    path('singup/',views.singup, name='singup'),
-    path('administrator/', views.administrator, name='admin' ),
-    path('singup/logout/',views.logout,name='logout'),
+    path('', views.iniciar, name="inicio"),
+    path('register/', views.register ),
+    path('singup/',views.singup),
+    path('administrator/', views.administrator ),
+    path('singup/logout/',views.logout),
     path('hc/', views.crearHistoriaClinica, name = 'hc' ),
     path('dueño-mascota/', views.crearDueñoMascota, name = 'dueño' ),
-    path('mascota/', views.crearMascota, name = 'mascota' ),
-    path('error/<str:user_id>/delete', views.eliminarUser, name = 'delete' ),
+    path('mascota/', views.crearMascota, name = 'mascota' )
 ]
